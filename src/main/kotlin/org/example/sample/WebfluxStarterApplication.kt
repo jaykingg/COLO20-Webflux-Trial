e7e0d53222cc00476a9f7f9bbd396d4f7698b2d3
jayking <mojunseo49@naver.com>
2024-04-26 17:25:36 +0900

    add: R2DBC 는 ORM 지원을 안함, Init schema 추가 및 running 시 테이블 생성하도록 추가

--- src/main/kotlin/org/example/sample/WebfluxStarterApplication.kt
+++ src/main/kotlin/org/example/sample/WebfluxStarterApplication.kt
@@ -1,11 +1,36 @@
 package org.example.sample
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+import org.springframework.web.reactive.config.EnableWebFlux
+import java.util.*
 
+
+@EnableWebFlux
 @SpringBootApplication
-class WebfluxStarterApplication
+class WebfluxStarterApplication {
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory?): ConnectionFactoryInitializer {
+        val initializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory!!)
+        initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema/Guide-Create.sql")))
+        return initializer
+    }
+
+
+}
 
 fun main(args: Array<String>) {
+    Locale.setDefault(Locale.KOREA)
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<WebfluxStarterApplication>(*args)
 }
+
+
+
+
