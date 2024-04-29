359fb06915d7b6fc0f55afa8bdf22621d723ac01
jayking <mojunseo49@naver.com>
2024-04-29 15:09:30 +0900

    fix: R2DBC Auditing 활성화(CreateDate, ModifiedDate)

--- src/main/kotlin/org/example/sample/WebfluxStarterApplication.kt
+++ src/main/kotlin/org/example/sample/WebfluxStarterApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.core.io.ClassPathResource
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import org.springframework.web.reactive.config.EnableWebFlux
@@ -12,6 +13,7 @@ import java.util.*
 
 
 @EnableWebFlux
+@EnableR2dbcAuditing
 @SpringBootApplication
 class WebfluxStarterApplication {
     @Bean
