1565aaee8af2ebb9d7c0bdc276fa6b6043d64888
jayking <mojunseo49@naver.com>
2024-05-03 13:41:18 +0900

    fix: Exception 추가

--- src/main/kotlin/org/example/sample/core/ExceptionsHandler.kt
+++ src/main/kotlin/org/example/sample/core/ExceptionsHandler.kt
@@ -5,17 +5,38 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.bind.support.WebExchangeBindException
 import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.ServerWebInputException
 
 @RestControllerAdvice
 @Order(-2)
 class ExceptionsHandler {
+    @ExceptionHandler(WebExchangeBindException::class)
+    fun handleValidationExceptions(ex: WebExchangeBindException): ErrorResponse {
+        val errorMessage = ex.bindingResult.fieldErrors.joinToString { it.defaultMessage ?: "" }
+        return ErrorResponse(
+            code = HttpStatus.BAD_REQUEST.value(),
+            error = "잘못된 요청입니다.",
+            message = errorMessage
+        )
+    }
+
+    @ExceptionHandler(ServerWebInputException::class)
+    fun handleValidationExceptions(ex: ServerWebInputException): ErrorResponse {
+        return ErrorResponse(
+            code = HttpStatus.BAD_REQUEST.value(),
+            error = "잘못된 요청입니다.",
+            message = ex.message
+        )
+    }
+
     @ExceptionHandler(BadRequestException::class)
     fun handleBadRequest(ex: BadRequestException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.BAD_REQUEST.value(),
-            error = HttpStatus.BAD_REQUEST.reasonPhrase,
-            message = "잘못된 요청입니다."
+            error = "잘못된 요청입니다.",
+            message = ex.message
         )
         return ResponseEntity(response, HttpStatus.BAD_REQUEST)
     }
@@ -24,7 +45,7 @@ class ExceptionsHandler {
     fun handleNotFound(ex: NotFoundException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.NOT_FOUND.value(),
-            error = HttpStatus.NOT_FOUND.reasonPhrase,
+            error = "리소스를 찾을 수 없습니다.",
             message = ex.message
         )
         return ResponseEntity(response, HttpStatus.NOT_FOUND)
@@ -34,7 +55,7 @@ class ExceptionsHandler {
     fun handleInternalError(ex: InternalServerException, exchange: ServerWebExchange): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(
             code = HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            error = HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
+            error = "서버에러가 발생했습니다.",
             message = ex.message
         )
         return ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR)
