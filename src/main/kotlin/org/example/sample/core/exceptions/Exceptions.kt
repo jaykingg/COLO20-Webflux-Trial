ce0cd52ec823bc93ee84f511ad3c648241191244
jayking <mojunseo49@naver.com>
2024-05-09 17:35:53 +0900

    feat: 불필요 Exception class 삭제

--- src/main/kotlin/org/example/sample/core/exceptions/Exceptions.kt
+++ src/main/kotlin/org/example/sample/core/exceptions/Exceptions.kt
@@ -1,11 +1,7 @@
-package org.example.sample.core
+package org.example.sample.core.exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
-class BadRequestException : ResponseStatusException(HttpStatus.BAD_REQUEST)
 class NotFoundException : ResponseStatusException(HttpStatus.NOT_FOUND)
 class InternalServerException : ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR)
-
-class ForbiddenException(message: String?) : RuntimeException(message)
-class UnauthorizedException(message: String?) : RuntimeException(message)
